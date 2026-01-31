ab3b35af937bdac3a1490ce835dabf464db6a390
Muhasif-Hafiz <muhasifhafiz@pilloo.ai>
2026-01-31 21:03:01 +0530

    First Test Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    for(int j=0;j<10;j++){
+        for(int i=j; i<=10;i++){
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+}
