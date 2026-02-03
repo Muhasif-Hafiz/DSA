1ef19c9a253f579686c83e89894ef154e41f8bbf
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2026-02-03 17:12:11 +0530

    Refacctored

--- src/maths/BasicMathProblems.java
+++ src/maths/BasicMathProblems.java
@@ -16,7 +16,7 @@ public class BasicMathProblems {
 
         // boolean isPalindrome = isNumberPalindrome(1122311);
          //System.out.println(Arrays.toString(printDivisors(6)));
-        // System.out.println( printDivisors(1000));
+         System.out.println( printDivisorsOptimised(36));
 
 
     }
@@ -97,11 +97,32 @@ public class BasicMathProblems {
 
         return oldNum ==newNum;
     }
+    static ArrayList printDivisors(int num){
+        ArrayList list = new ArrayList<>();
 
+        for(int i=1;i<=num;i++){
+            if(num % i ==0){
+                list.add(i);
+            }
 
+        }
+        return list;
+    }
+    static  ArrayList printDivisorsOptimised(int num){
+        ArrayList list = new ArrayList<>();
 
-
-
+        for(int i=1;i*i<=num;i++){
+            if(num % i ==0){
+               if(i == num/i){
+                   list.add(i);
+               }else{
+                   list.add(i);
+                   list.add(num/i);
+               }
+            }
+        }
+        return list;
+    }
 
 }
 
