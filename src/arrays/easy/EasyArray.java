9bb0a267bb0c8ea613a2d771163056163e8da16b
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2026-02-11 20:56:07 +0530

    11 Feb 2026

--- src/arrays/easy/EasyArray.java
+++ src/arrays/easy/EasyArray.java
@@ -1,10 +1,25 @@
 package arrays.easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+
 public class EasyArray {
     static void main() {
-        int[] arr = {1,2,3,4,5,6,6,6,7,7};
-        // System.out.println(secondLargestElement(arr));
-        System.out.println(removeDuplicates(arr));
+//        int[] arr = {0,0,0,1,0,0,2,0,0,4,0};
+//        // System.out.println(secondLargestElement(arr));
+//        moveZeroesOptimal(arr);
+//        System.out.println(Arrays.toString(arr));
+////
+//        int[] nums1 = {3, 4, 6, 7, 9, 9};
+//        int[] nums2 = {1, 5, 7, 8, 8};
+//        int [] ans = unionArray(nums1, nums2);
+//        System.out.println(Arrays.toString(ans));
+     // int [] ans=  unionArrayOptimal(nums1, nums2);
+
+        int [] arr = {8, 2, 4, 5, 3, 7, 1};
+        System.out.println(findMissingNumber(arr));
 
     }
 
@@ -70,14 +85,188 @@ public class EasyArray {
 
         int i = 0;
         int j = i + 1;
-        while (i < nums.length-1) {
-            while ( j<nums.length && nums[j] == nums[i]) {
+        while (i < nums.length - 1) {
+            while (j < nums.length && nums[j] == nums[i]) {
                 j++;
             }
-            if(j==nums.length)break;
+            if (j == nums.length) break;
             i++;
             nums[i] = nums[j];
         }
-        return i+1;
+        return i + 1;
     }
+
+    static void reverse(int[] arr, int low, int high) {
+
+        int mid = low + (high - 1) / 2;
+
+        while (low <= mid && low < high) {
+            swap(arr, low, high);
+            low++;
+            high--;
+        }
+
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    static int[] rotateBrute(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+
+        k = k % n;
+        int j = 0;
+
+        for (int i = n - k; i < n; i++) {
+            newArr[j++] = nums[i];
+        }
+
+        for (int i = 0; i < n - k; i++) {
+            newArr[j++] = nums[i];
+        }
+
+        return newArr;
+    }
+
+    public static void moveZeroes(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                list.add(nums[i]);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+        for (int i = list.size(); i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void moveZeroesOptimal(int[] nums) {
+        int i = 0;
+        int j = 0;
+
+        while (i < nums.length) {
+            while (nums[j] == 0) {
+                if (j < nums.length - 1) {
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            swap(nums, i, j);
+            i++;
+            if (nums[j] != 0) j++;
+        }
+    }
+
+    public static int[] unionArray(int[] nums1, int[] nums2) {
+
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            set.add(nums2[i]);
+        }
+        int[] result = new int[set.size()];
+        int i = 0;
+
+        for (int num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    public static int [] unionArrayOptimal(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        int prev = Integer.MIN_VALUE;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                if (prev != nums1[i]) {
+                    list.add(nums1[i]);
+                    prev = nums1[i];
+                }
+                i++;
+            } else {
+                if (prev != nums2[j]) {
+                    list.add(nums2[j]);
+                    prev = nums2[j];
+                }
+                j++;
+            }
+
+        }
+
+        while (i < nums1.length) {
+            if (prev != nums1[i]) {
+                list.add(nums1[i]);
+                prev = nums1[i];
+            }
+            i++;
+        }
+        while (j < nums2.length) {
+            if (prev != nums2[j]) {
+                list.add(nums2[j]);
+                prev = nums2[j];
+            }
+            j++;
+        }
+        int[] newArr = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            newArr[k] = list.get(k);
+        }
+
+        return newArr;
+    }
+
+    static int findMissingNumberBetter(int [] arr){
+        boolean [] hashArray  = new boolean [arr.length+1];
+
+        for(int i=0;i<arr.length;i++){
+            int index = arr[i]-1;
+            hashArray[index] = true;
+        }
+        int ans =0;
+        for(int i=1;i<hashArray.length;i++){
+            if(!hashArray[i]){
+                ans = i;
+            }
+        }
+
+        return ans+1;
+    }
+    static int findMissingNumber(int [] arr){
+
+        long n = arr.length+1;
+        long sum =0;
+
+        for(int i=0;i<arr.length;i++){
+            sum +=arr[i];
+        }
+        long expectedSum = n *(n+1)/2;
+
+
+        return  (int) (expectedSum-sum);
+
+    }
+
 }
