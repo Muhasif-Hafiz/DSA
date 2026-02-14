48b99e862f800985cf9435af308b37b282f519c7
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2026-02-14 21:24:28 +0530

    2 sum

---
+++ src/arrays/medium/MediumArray.java
@@ -0,0 +1,86 @@
+package arrays.medium;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class MediumArray {
+    static void main() {
+
+        int[] arr = {3, 2, 4};
+        System.out.println(Arrays.toString(twoSum(arr, 6)));
+    }
+
+    public static int[] twoSumBrute(int[] nums, int target) {
+
+        int[] ansArr = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    ansArr[0] = i;
+                    ansArr[1] = j;
+                }
+            }
+        }
+        return ansArr;
+
+    }
+
+    public static int[] twoSumBetter(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int rem = target - nums[i];
+            if (map.containsKey(rem)) {
+                return new int[]{i, map.get(rem)};
+            } else {
+                map.put(nums[i], i);
+            }
+
+        }
+        return new int[]{-1, -1};
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int [] originalArr = nums;
+
+
+        int i = 0;
+        int j = nums.length - 1;
+        int[] arr = new int[2];
+
+
+        Arrays.sort(originalArr);
+        while (i < originalArr.length && j > 0) {
+
+            long sum = originalArr[i] + originalArr[j];
+
+            if (sum > target) {
+                j--;
+            } else if (sum < target) {
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        for (int k = 0; k < nums.length; k++) {
+
+            if (nums[k] == originalArr[i]) {
+                arr[0] = k;
+            }
+            if (nums[k] == originalArr[j]) {
+                arr[1] = k;
+            }
+
+        }
+
+        return arr;
+
+    }
+}
