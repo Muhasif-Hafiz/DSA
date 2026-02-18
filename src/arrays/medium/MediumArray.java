a067d1e3565c23057826259c030151b0cbcf59e1
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2026-02-18 22:50:53 +0530

    majority element

--- src/arrays/medium/MediumArray.java
+++ src/arrays/medium/MediumArray.java
@@ -10,9 +10,9 @@ public class MediumArray {
 //        int[] arr = {3, 2, 4};
 //        System.out.println(Arrays.toString(twoSum(arr, 6)));
 
-        int []arr ={1,2,0,1,2,0,0};
-        sortColors(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {7, 0, 0, 1, 7, 7, 2, 7, 7};
+      //  sortColors(arr);
+        System.out.println(majorityElementBetter(arr));
     }
 
     public static int[] twoSumBrute(int[] nums, int target) {
@@ -52,7 +52,7 @@ public class MediumArray {
 
     public static int[] twoSum(int[] nums, int target) {
 
-        int [] originalArr = nums;
+        int[] originalArr = nums;
 
 
         int i = 0;
@@ -90,29 +90,30 @@ public class MediumArray {
         // incomplete
 
     }
+
     public static void sortColorsBrute(int[] nums) {
 
-        int count0=0;
-        int count1=0;
-        int count2 =0;
-        int k=0;
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        int k = 0;
 
-        for(int i=0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
 
-            if(nums[i]== 0) count0++;
-            else if(nums[i]==1) count1++;
+            if (nums[i] == 0) count0++;
+            else if (nums[i] == 1) count1++;
             else count2++;
 
         }
 
-        for(int i=0;i<count0;i++){
-            nums[k++]=0;
+        for (int i = 0; i < count0; i++) {
+            nums[k++] = 0;
         }
-        for(int i=0;i<count1;i++){
-            nums[k++]=1;
+        for (int i = 0; i < count1; i++) {
+            nums[k++] = 1;
         }
-        for(int i=0;i<count2;i++){
-            nums[k++]=2;
+        for (int i = 0; i < count2; i++) {
+            nums[k++] = 2;
         }
     }
 //    public static void sortColorsBetter(int[] nums){
@@ -129,21 +130,21 @@ public class MediumArray {
 //        }
 //    }
 
-    public static void sortColors(int [] nums){
-        int low =0;
-        int mid =0;
-        int high =nums.length-1;
+    public static void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
 
-        while (mid<=high){
-            if(nums[mid]==0){
+        while (mid <= high) {
+            if (nums[mid] == 0) {
                 swap(nums, low, mid);
                 low++;
                 mid++;
 
-            } else if(nums[mid]==1){
+            } else if (nums[mid] == 1) {
                 mid++;
 
-            }else{
+            } else {
                 swap(nums, mid, high);
                 high--;
 
@@ -151,10 +152,48 @@ public class MediumArray {
         }
 
     }
+
     static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    public static int majorityElementBrute(int[] nums) {
+        int n = nums.length;
+
+        int majority = n/2;
+        for(int i=0;i<n;i++){
+            int count=0;
+
+            for(int j=0;j<n;j++){
+                if(nums[i]== nums[j]){
+                    count++;
+                }
+            }
+            if(count>majority){
+                return nums[i];
+            }
+
+        }
+
+        return -1;
+
+    }
+    public static int majorityElementBetter(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int majority= nums.length/2;
+
+        for(int i=0;i< nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i], 0)+1 );
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue()>majority){
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 
 
