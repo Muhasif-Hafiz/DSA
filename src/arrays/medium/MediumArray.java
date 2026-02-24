61faeb2ee7f27b1f8ed58709ae9dc3c94a1b3050
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2026-02-24 23:09:53 +0530

    Set Zeroes - BruteForce

--- src/arrays/medium/MediumArray.java
+++ src/arrays/medium/MediumArray.java
@@ -5,20 +5,33 @@ import java.util.*;
 public class MediumArray {
     static void main() {
 
-//        int[] arr = {3, 2, 4};
-//        System.out.println(Arrays.toString(twoSum(arr, 6)));
+////        int[] arr = {3, 2, 4};
+////        System.out.println(Arrays.toString(twoSum(arr, 6)));
+//
+//        int[] arr = {16, 17, 4, 3, 5, 2};
+//
+//        //  sortColors(arr);
+////        System.out.println(maxProfit(arr));
+////        int [] ans = maxSubArrayPart(arr);
+////        nextPermutation(arr);
+//
+//        List<Integer> list = leaders(arr);
+//
+//
+//        System.out.println(list);
 
-        int[] arr = {16, 17, 4, 3, 5, 2};
 
-        //  sortColors(arr);
-//        System.out.println(maxProfit(arr));
-//        int [] ans = maxSubArrayPart(arr);
-//        nextPermutation(arr);
+        int[][] ans = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
 
-        List<Integer> list = leaders(arr);
+        setZeroesBruteForce(ans);
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
 
 
-        System.out.println(list);
     }
 
     public static int[] twoSumBrute(int[] nums, int target) {
@@ -554,6 +567,46 @@ public class MediumArray {
 
         Collections.reverse(list);
         return list;
+
+    }
+    public static void setZeroesBruteForce(int[][] matrix) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+
+        int[][] result = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    alterMatrix(result, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = result[i][j];
+            }
+        }
+    }
+    static void alterMatrix(int [] [] result, int i, int j){
+
+        for(int row =0;row<result.length;row++){
+            for(int col =0; col <result[0].length;col++){
+
+                if(row ==i || col ==j ){
+                    result[row][col] =0;
+                }
+            }
+        }
 
     }
 }
