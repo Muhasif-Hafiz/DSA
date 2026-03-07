effe5b967b6fa099d5eb8a025a11ec44ca2cb83e
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2026-03-07 22:18:54 +0530

    Merge Overlapping Intervals Optimal

--- src/arrays/hard/ArraysHard.java
+++ src/arrays/hard/ArraysHard.java
@@ -6,12 +6,19 @@ public class ArraysHard {
     static void main() {
 
 
-        int[] arr = {5, 2, 9};
-//        List<List<Integer>> list = fourSum(arr, 0);
-//        System.out.println(list);
+        int[] [] arr = {{1,3},{2,6},{8,10},{15,18}};
 
-        // System.out.println(factorial(4,2));
-        System.out.println(subarraysWithXorK(arr, 7));
+
+
+        int [] [] ans = merge(arr);
+
+        for(int i=0;i<ans.length;i++)
+        {
+            for(int j = 0;j<ans[0].length;j++){
+                System.out.print(ans[i][j]);
+            }
+            System.out.println();
+        }
     }
 
     public static List<List<Integer>> generatePascalBrute(int numRows) {
@@ -325,7 +332,9 @@ public class ArraysHard {
     }
 
     public static int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals);
+        Arrays.sort(intervals,
+                (a,b) -> Integer.compare(a[0], b[0])
+                );
         List<List<Integer>> merged = new ArrayList<>();
 
         for (int [] interval : intervals){
