4f19746320671d96a4b9c98cb80e40c83fae4e82
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2026-08-25 23:26:11 +0530

    rightAngledNumberPyramidPattern

--- src/patterns/BasicPatterns.java
+++ src/patterns/BasicPatterns.java
@@ -1,7 +1,9 @@
 
 void main() {
     //rectangularStarPattern(5);
-    rightAngledTrianglePattern(5);
+    //rightAngledTrianglePattern(5);
+    //dualLoop(0, 5, 0, 5, '*');
+    rightAngledNumberPyramidPattern(5);
 }
 
 
@@ -24,3 +26,21 @@ public static void rightAngledTrianglePattern(int n ){
     }
 }
 
+public static void dualLoop(int startI, int endI, int startJ, int endJ, char c){
+    for(int i=startI;i<endI;i++){
+        for(int j = startJ; j<endJ; j++){
+            System.out.print(c);
+        }
+        System.out.println();
+    }
+}
+public static void  rightAngledNumberPyramidPattern(int n){
+
+
+    for(int i =1;i<=n ;i ++){
+        for(int j =1;j<=i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
